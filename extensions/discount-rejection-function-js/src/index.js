8959922f5a8d50676f432f5102db909a48495edc
Andrea Solis <acsolis896@gmail.com>
2026-06-26 13:16:36 -0400

    Fix: use shopify app function build command matching working extension pattern

--- extensions/discount-rejection-function-js/src/index.js
+++
@@ -1,8 +0,0 @@
-import run from "@shopify/shopify_function/run";
-import { cartLinesDiscountsGenerateRun as userFunction } from "./cart_lines_discounts_generate_run.js";
-
-function cartLinesDiscountsGenerateRun() {
-  return run(userFunction);
-}
-
-export { cartLinesDiscountsGenerateRun };
