ca43cfd6eec3efa33b78b114164522865e6b92e5
Andrea Solis <acsolis896@gmail.com>
2026-06-26 12:53:56 -0400

    Fix build pipeline: esbuild from src/index.js entry with Shopify wrapper

--- extensions/discount-rejection-function-js/src/index.js
+++ extensions/discount-rejection-function-js/src/index.js
@@ -1,1 +1,8 @@
-export * from './cart_lines_discounts_generate_run';
+import run from "@shopify/shopify_function/run";
+import { cartLinesDiscountsGenerateRun as userFunction } from "./cart_lines_discounts_generate_run.js";
+
+function cartLinesDiscountsGenerateRun() {
+  return run(userFunction);
+}
+
+export { cartLinesDiscountsGenerateRun };
